bd7b95bca10c3a9e52c6e7c1c8efbf9d0886593a
lautaro perez <lauttiiperez@gmail.com>
2025-04-14 23:03:31 -0300

    Llamada  mediante fetch a la imagen con la primera palabra del hecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react"
 
 function App() {
-  const [fact, setFact] = useState("")
-
+  const [fact, setFact] = useState()
+  const [image, setImage] = useState()
   useEffect(()=>{
     fetch("https://catfact.ninja/fact")
       .then(res => res.json())
-      .then(data => setFact(data.fact))
-  },[])
+      .then(data =>{
+       const {fact }= data
+        setFact(fact)
+        
+        const firstWord = fact.split(" ")[0]
+        fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+        .then(res => res.json())
+        .then(response => {
+          const { url } = response
+          setImage(url)
+        })
+      })
+    },[])
 
   return (
     <main>
      <h1>App Gatitos</h1>
-     <p>{fact}</p>
+     {fact && <p>{fact}</p>}
+     {image && <img src={image} alt={`Image extracted using first word for ${fact}`} />}
     </main>
   )
 }
