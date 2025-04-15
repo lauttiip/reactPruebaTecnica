ec3921951ea17cda42cf319c79e399852dfa5a3a
lautaro perez <lauttiiperez@gmail.com>
2025-04-14 23:36:30 -0300

    separar lógica de fetch en useEffects independientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
   const [fact, setFact] = useState()
@@ -8,23 +9,28 @@ function App() {
       .then(res => res.json())
       .then(data =>{
        const {fact }= data
-        setFact(fact)
-        
-        const firstWord = fact.split(" ")[0]
-        fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
-        .then(res => res.json())
-        .then(response => {
-          const { url } = response
-          setImage(url)
-        })
-      })
+        setFact(fact)})
     },[])
+
+  useEffect(()=>{
+    if(!fact) return
+    const firstWord = fact.split(" ")[0]
+    fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+    .then(res => res.json())
+    .then(response => {
+      const { url } = response
+      setImage(url)
+    })
+  })
 
   return (
     <main>
-     <h1>App Gatitos</h1>
-     {fact && <p>{fact}</p>}
-     {image && <img src={image} alt={`Image extracted using first word for ${fact}`} />}
+      <h1>App Gatitos</h1>
+      <section>
+        {fact && <p>{fact}</p>}
+        {image && <img src={image} alt={`Image extracted using first word for ${fact}`} />}
+        
+      </section>
     </main>
   )
 }
